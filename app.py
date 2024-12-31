e1457a363dcb484c1797650bf3ed3f74e05c0d24
marwanazd <azdoudmarwan20@gmail.com>
2024-12-31 17:00:56 +0100

    port

--- app.py
+++ app.py
@@ -451,7 +451,7 @@ def remove_drawing():
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5000, debug=False)
+        app.run(host='0.0.0.0', port=5010, debug=False)
     finally:
         # Stop processing thread gracefully when the application exits
         processing_active = False
